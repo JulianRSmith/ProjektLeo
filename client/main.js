d89087b5743e540ea744d69e5b139966f041ba8f
Ashley Scott <admin@projectge.com>
2017-02-25 18:38:21 +0000

    Phaser can now connect to a server and get a message back

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@
 
 // Get the width of the users screen
 var screenWidth = window.screen.width;
+
 // Set up game enviornemnt
 var game = new Phaser.Game(screenWidth,600, Phaser.AUTO, 'gameViewpoint');
 
--- client/menu.js
+++ client/menu.js
@@ -2,8 +2,13 @@ var menuState = {
     
     create: function () {
         console.log("Game - Loaded");
-        menuText = game.add.text(0,0,'Welcome',{ font: "40px Arial", fill: "#ffffff",boundsAlignH: "center", boundsAlignV: "middle" });
+
+        menuText = game.add.text(0, 0, 'ProjektLeo', { font: "40px Arial", fill: "#FFFFFF", boundsAlignH: "center", boundsAlignV: "middle" });
         menuText.setTextBounds(0, 100, screenWidth, 100);
+
+        debugText = game.add.text(0, 0, 'Connecting to server...', { font: "24px Arial", fill: "#FF4C4C", boundsAlignH: "center", boundsAlignV: "middle" });
+        debugText.setTextBounds(0, 192, screenWidth, 100);
+
+		setTimeout(function() { connector.connect("127.0.0.1", "3010"); }, 2000);
     }
-    
 }
