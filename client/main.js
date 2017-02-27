99de85879c9aec9c524e47a1713a4b52ed1de318
JulianRSmith <juliansmith95@hotmail.com>
2017-02-27 12:43:54 +0000

    Fixed bugs from combining different development timelines

--- client/main.js
+++ client/main.js
@@ -12,11 +12,7 @@ var game = new Phaser.Game(screenWidth,600, Phaser.AUTO, 'gameViewpoint');
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
-game.state.add('play', playState);
+// game.state.add('play', playState);
 
-<<<<<<< HEAD:js/main.js
 // Call boot state
 game.state.start('boot');
-=======
-game.state.start('boot');
->>>>>>> d89087b5743e540ea744d69e5b139966f041ba8f:client/main.js
