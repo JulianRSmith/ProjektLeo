a67a59700cd32650e52211ca0b23db3551a0b1fe
Ashley Scott <admin@projectge.com>
2017-03-08 02:27:31 +0000

    Allows client to create lobbies on the server

--- server/game-server/app/servers/connector/handler/entryHandler.js
+++ server/game-server/app/servers/connector/handler/entryHandler.js
@@ -6,6 +6,24 @@ var Handler = function(app) {
 	this.app = app;
 };
 
+var lobbyList = [];
+
 Handler.prototype.entry = function(msg, session, next) {
+	console.log(session);
 	next(null, {code: 200, msg: 'Welcome to the server!'});
 };
+
+Handler.prototype.getLobbies = function(msg, session, next) {
+	next(null, 
+		{
+			code: 200, 
+			lobbyCount: lobbyList.length,
+			lobbyData: lobbyList
+		}
+	)
+}
+
+Handler.prototype.createLobby = function(msg, session, next){
+	lobbyList.push({name: "New Lobby", slots: 2, players: []});
+	next(null, {code: 200, name: "New Lobby", slots: 2, players: []});
+}
