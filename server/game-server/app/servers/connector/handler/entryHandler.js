8c87227134d0180a7ca6f0c9e598e192bdea9ead
Ashley Scott <admin@projectge.com>
2017-03-29 04:11:47 +0100

    Clean server code and send error flag if the action caused an error

--- server/game-server/app/servers/connector/handler/entryHandler.js
+++ server/game-server/app/servers/connector/handler/entryHandler.js
@@ -9,27 +9,47 @@ var Handler = function(app) {
 var lobbyList = [];
 
 Handler.prototype.onEntry = function(msg, session, next) {
-	console.log(session);
-	next(null, {msg: 'Welcome to the server!'});
+	
+	next(null, { error: false, msg: "Welcome to the server!" });
+
 };
 
 Handler.prototype.onGetLobbies = function(msg, session, next) {
-	next(null, { lobbyCount: lobbyList.length, lobbyData: lobbyList});
+
+	next(null, { error: false, lobbyCount: lobbyList.length, lobbyData: lobbyList});
+
 }
 
 Handler.prototype.onCreateLobby = function(msg, session, next){
+
 	var lobby = {id: lobbyList.length, name: msg.lobbyName, host: msg.lobbyHost, slots: 2, players: []};
 
 	lobbyList.push(lobby);
 	
-	next(null, lobby);
+	next(null, { error: false, lobby: lobby });
+
+}
+
+Handler.prototype.onEnterLobby = function(msg, session, next) {
+
+	if(lobbyList[msg.lobbyId]) { 
+		next(null, { error: false, lobby: lobbyList[msg.lobbyId] });
+	}
+	else {
+		next(null, { error: true, msg: "Lobby does not exist." });
+	}
+
 }
 
 Handler.prototype.onGetPing = function(msg, session, next) {
-	next(null, {msg: "ping"});
+
+	next(null, { error:: false, msg: "ping" });
+
 }
 
-Handler.prototype.onDisconnect = function(msg, session, next){
-	next(null, {msg: "Disconnect requested."});
+Handler.prototype.onDisconnect = function(msg, session, next) {
+
+	next(null, { error: false, msg: "Disconnect requested." });
 	kickBySessionId(session);
+
 }
